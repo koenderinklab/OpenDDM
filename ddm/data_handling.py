601aea5d5139fa1d10f28ab0603b9f70c7fca985
imuntz <i.a.a.muntz@tudelft.nl>
2022-05-30 14:03:07 +0200

    added readLif function to image_loading file in ddm

--- ddm/data_handling.py
+++ ddm/data_handling.py
@@ -9,3 +9,19 @@ def readND2(filename):
     sequence.attrs = {'xyScale': float(scale),
                       'tScale': frameT}
     return(sequence)
+
+def readLIF(filename):
+    """
+    
+    """
+    lifSeq = pims.Bioformats('data/21-03-31_ddm.lif', read_mode = 'jpype')
+    xscale = lifSeq.metadata.PixelsPhysicalSizeX(0)
+    tscale = lifSeq.metadata.PlaneDeltaT(0,1)
+    lifXCoords = np.arange(0., lifSeq.shape[1]*xscale, xscale)
+    lifYCoords = np.arange(0., lifSeq.shape[2]*xscale, xscale)
+    lifTCoords = np.arange(0., lifSeq.shape[0]*tscale, tscale)
+    ds = da.from_array(lifSeq)
+    sequence = xarray.DataArray(data = ds, dims = ["T", "Y", "X"], 
+                                coords = (lifTCoords, lifYCoords, lifXCoords), 
+                                attrs = dict(xyScale = xscale, tScale = tscale))
+    return sequence
