e903d52fc43ad44b0ea14693ca236b14f0239d04
imuntz <i.a.a.muntz@tudelft.nl>
2022-05-31 09:31:54 +0200

    added comments to the functions for loading in .nd2 and .lif following the numpy docstring format

--- ddm/data_handling.py
+++ ddm/data_handling.py
@@ -1,6 +1,14 @@
 def readND2(filename):
-    """
+    """A function to read in a .nd2 file taken from a Nikon microscope. Has been shown to work for files from our Minicell in the Koenderink lab.
     
+    Parameters
+    ----------
+    filename : string
+               the path and name of the .nd2 file which is to be loaded in
+    Returns
+    -------
+    dask array
+        array with the image data saved as a dask array. Metadata in the dask array includes the spatial resolution in microns per pixel and the temporal resolution in milliseconds per frame.
     """
     sequence = nd2.imread(filename, xarray = True, dask = True)
     frameT = sequence.metadata['experiment'][0].parameters.periodDiff.avg #this is the time per frame in ms
@@ -11,7 +19,16 @@ def readND2(filename):
     return(sequence)
 
 def readLIF(filename):
-    """
+    """A function to read in a .lif file taken from a Leica microscope. Has been shown to work for files from our Thunder in the Koenderink lab.
+    
+    Parameters
+    ----------
+    filename : string
+               the path and name of the .lif file which is to be loaded in
+    Returns
+    -------
+    dask array
+        array with the image data saved as a dask array. Metadata in the dask array includes the spatial resolution in microns per pixel and the temporal resolution in milliseconds per frame.
     
     """
     lifSeq = pims.Bioformats('data/21-03-31_ddm.lif', read_mode = 'jpype')
