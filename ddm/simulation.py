d874dee0ce85526747965e260809b1e5a7021f91
Maurits Kok <m.w.a.kok@tudelft.nl>
2022-07-26 19:43:16 +0200

    Update simulation scripts

--- ddm/simulation.py
+++ ddm/simulation.py
@@ -1,12 +1,15 @@
-import numpy as np
-from tifffile import imwrite
-from skimage import util
-from sdt.sim import simulate_gauss
 from typing import Tuple
+
+import numpy as np
+from sdt.sim import simulate_gauss
+from skimage import util
+from tifffile import imwrite
 from tqdm import tqdm
 
 
-def generate_stack(tracks: np.array) -> np.array:
+def generate_stack(
+    tracks: np.ndarray, window: Tuple[int, int] = (512, 512)
+) -> np.ndarray:
     """Create an image stack of simulated microscopy images
 
     Parameters
@@ -21,7 +24,7 @@ def generate_stack(tracks: np.array) -> np.array:
     """
     stack = []
     for i in tqdm(range(tracks.shape[-1])):
-        frame = util.invert(generate_frame(tracks[:, :, i]))
+        frame = util.invert(generate_frame(tracks[:, :, i], window))
         stack.append(frame)
     return np.asarray(stack)
 
@@ -30,10 +33,10 @@ def generate_tracks(
     n_particles: int = 100,
     steps: int = 1000,
     tau: float = 0.01,
-    drift: Tuple[float, float] = (-0.1, -0.1),
+    drift: Tuple[float, float] = (0.1, 0.1),
     window: Tuple[int, int] = (512, 512),
     pixel_size: float = 1e-7,
-) -> np.array:
+) -> np.ndarray:
     """Generate tracks of particles undergoing 2D Brownian motion
 
     Parameters
@@ -43,13 +46,13 @@ def generate_tracks(
     steps : int, optional
         number of time points to be simulated, by default 1000
     tau : float, optional
-        time between steps, by default 0.01 [in seconds]
+        time between steps in seconds, by default 0.01
     drift : Tuple[float, float], optional
-        drift in x and y direction, by default (-0.1, -0.1) [fraction of step size]
+        drift in x and y direction in fraction of step size, by default (-0.1, -0.1)
     window : Tuple[int, int], optional
-        size of the simulated image, by default (512, 512) [in pixels]
+        size of the simulated image in pixels, by default (512, 512)
     pixel_size : float, optional
-        size of the pixel, by default 1e-7 [in meters]
+        size of the pixel in meters, by default 1e-7
 
     Returns
     -------
@@ -67,15 +70,17 @@ def generate_tracks(
     window_extended = tuple(np.round(x * 2) for x in window)
     coords = np.array(
         [
-            np.random.uniform(0, window_extended[0], n_particles),
-            np.random.uniform(0, window_extended[1], n_particles),
+            np.random.uniform(-window_extended[0], window_extended[0], n_particles),
+            np.random.uniform(-window_extended[0], window_extended[1], n_particles),
         ]
     ).T
 
     return np.dstack((coords, z)).cumsum(axis=2)
 
 
-def generate_frame(coords: np.array, window: Tuple[int, int] = (512, 512)) -> np.array:
+def generate_frame(
+    coords: np.ndarray, window: Tuple[int, int] = (512, 512)
+) -> np.ndarray:
     """Create microscopy image from tracks at single timepoint
 
     Parameters
@@ -83,56 +88,61 @@ def generate_frame(coords: np.array, window: Tuple[int, int] = (512, 512)) -> np
     coords : np.array
         coordinates of particles
     window : Tuple[int, int], optional
-        size of the simulated image, by default (512, 512) [in pixels]
+        size of the simulated image in pixels, by default (512, 512)
 
     Returns
     -------
     np.array
         2D array of microscope image with particles
     """
-    amplitudes = 500
-    sigmas = 5
+    amplitudes = 500  # amplitude of the PSF
+    sigmas = 5  # sigma of the amplitude
     img = simulate_gauss(window, coords, amplitudes, sigmas)
     img_noisy = np.array(np.random.poisson(img), dtype="float16")  # add shot noise
     img_noisy += np.random.normal(200, 20, img_noisy.shape)  # add camera noise
     return img_noisy
 
 
-def get_diffusion_coefficient():
+def get_diffusion_coefficient(
+    particle_size: float = 1e-6, eta: float = 1e-3, T: float = 293
+) -> float:
     """Calculate diffusion coefficient
+
+    Parameters
+    ----------
+    particle_size : float
+        size of the particles in meters, defaults to 1e-6
+    eta : float
+        viscosity of the medium in Pascal-seconds, defaults to 1e-3
+    T : float
+        temperature in Kelvin, defaults to 293
 
     Returns
     -------
     float
         diffusion coefficient
     """
-    particle_size = 1e-6  # in meter
-    eta = 1e-3  # viscosity (Pascal-seconds)
     kB = 1.38e-23  # Boltzmann constant
-    T = 293  # in Kelvin
-    D = kB * T / (3 * np.pi * eta * particle_size)  # Diffusion coefficient
-    return D
+    return kB * T / (3 * np.pi * eta * particle_size)
 
 
-def save_stack(stack: np.array, path: str = "diffusion.tif"):
+def save_stack(file_path: str, stack: np.ndarray):
     """Save image stack
 
     Parameters
     ----------
     stack : np.array
         3D array of simulated microscopy images
-    path : str, optional
+    file_path : str, optional
         path to save file, by default "diffusion.tif"
     """
-    imwrite(path, stack)
+    imwrite(file_path, stack)
 
 
 if __name__ == "__main__":
-    print("Loading simulation settings from ddm/sim_config.yml")
-
     print("Generating images...")
-    tracks = generate_tracks()
+    tracks = generate_tracks(n_particles=1000, steps=2000, drift=(0.0, 0.0))
     stack = generate_stack(tracks)
     print("Saving image stack...")
-    save_stack(stack)
+    save_stack("diffusion.tif", stack)
     print("Done!")
